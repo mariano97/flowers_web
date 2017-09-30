2275af4c1bf3e09ae0d346a1e8022cef98ddbc99
Mario Andres Serrano Urueña <mariano970419@gmail.com>
2017-09-29 21:29:47 -0500

    Add files via upload

---
+++ flask01.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+
+from flask import Flask
+import pickle
+  
+import sys
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+ 
+    
+app = Flask(__name__)
+
+#@app.route("/<username>")
+#def home(username):
+    
+@app.route("/<sl>/<sw>/<pl>/<pw>")
+def home(sl,sw,pl,pw):
+    
+    filename = "C:\Users\MARIO\Documents\python-iot\ejemplo_modelo.mod"
+    modelo_svm = pickle.load(open(filename, 'rb'))
+    
+    
+    """
+    sepal_length = 5.0
+    sepal_width = 3.5
+    petal_length = 1.3
+    petal_width = 0.3
+    5.0/3.5/1.3/0.3
+    """
+    sepal_length = sl
+    sepal_width = sw
+    petal_length = pl
+    petal_width = pw
+    
+    clase = modelo_svm.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+    #print(clase)
+    #return "Hello Guest! " + username
+    return str(clase)
+    
+    return filename    
+
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ sketch_apr11a.ino
@@ -0,0 +1,28 @@
+long valor1, valor2, valor3, valor4;
+
+
+void setup() {
+
+    Serial.begin(9600);
+
+}
+
+void loop() {
+
+  valor1 = analogRead(A0);
+  valor2 = analogRead(A1);
+  valor3 = analogRead(A2);
+  valor4 = analogRead(A3);
+
+
+
+  String sl = String(((6.0/1705.0)*valor1)+4.3);
+  String sw = String(((2.4/1023.0)*valor2)+2.0);
+  String pl = String(((59.0/10230.0)*valor3)+1.0);
+  String pw = String(((4.0/1705.0)*valor4)+0.1);
+ // Serial.println(String(sw) +" " + String (valor));
+ 
+  Serial.println("{\"sl\":" + sl + ", \"sw\":" + sw +", \"pl\":" + pl + ", \"pw\":" + pw + "}");
+ 
+  delay(3000);
+}
